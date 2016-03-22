ee8d23ad4d7be4bb040577196f50937beec6c0a1
omgimanerd <alvin.lin.dev@gmail.com>
2016-03-22 12:59:17 -0400

    knobs and dinguses

--- hw05/index.js
+++ hw05/index.js
@@ -1,14 +1,40 @@
-/** 
+/**
  * Main script for page.
  * @author alvin.lin.dev@gmail.com (Alvin Lin)
  */
 
 window.addEventListener('load', function() {
   var container = d3.select('#container');
-  var graph = container.selectAll('div').data(delegates).enter();
+  var republicans = true;
+
+  var getScaledWidth = d3.scale.linear()
+    .domain([0, 500])
+    .range([0, 800]);
+  var graph = container.selectAll('div').data(delegateData).enter();
+
   graph.append('div').text(function(data, index) {
-    return states[index];
+    return data.state;
   }).style('width', function(data, index) {
-    return Math.round(data * 6) + 'px';
+    return getScaledWidth(data.republicanDelegates) + 'px';
+  }).style('background-color', '#0000bb');
+
+  document.getElementById('switch').addEventListener('click', function() {
+    container.selectAll('div').data(delegateData)
+      .transition().duration(1000)
+      .style('width', function(data, index) {
+        if (republicans) {
+          return getScaledWidth(data.democratDelegates) + 'px';
+        }
+        return getScaledWidth(data.republicanDelegates) + 'px';
+      })
+      .style('background-color', function() {
+        if (republicans) {
+          return '#bb0000';
+        }
+        return '#0000bb';
+      });
+
+
+    republicans = !republicans;
   });
 });
