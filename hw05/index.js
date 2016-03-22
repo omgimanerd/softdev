1386964b4bbca117cdd1759c74d488ee99e35257
omgimanerd <alvin.lin.dev@gmail.com>
2016-03-22 13:20:49 -0400

    more doots and dings

--- hw05/index.js
+++ hw05/index.js
@@ -10,17 +10,34 @@ window.addEventListener('load', function() {
   var getScaledWidth = d3.scale.linear()
     .domain([0, 500])
     .range([0, 1000]);
-  var graph = container.selectAll('div').data(delegateData).enter();
 
-  graph.append('div').text(function(data, index) {
-    return data.state;
-  }).style('width', function(data, index) {
-    return getScaledWidth(data.republicanDelegates) + 'px';
-  }).style('background-color', '#0000bb');
+  var graph = container.selectAll('div')
+    .data(delegateData)
+    .enter()
+    .append('div')
+    .attr('class', 'state-container');
+
+  container.selectAll('.state-container')
+    .data(delegateData)
+    .append('div')
+    .attr('class', 'state-label')
+    .text(function(data) {
+      return data.state;
+    });
+
+  container.selectAll('.state-container')
+    .data(delegateData)
+    .append('div')
+    .attr('class', 'state-bar')
+    .style('width', function(data, index) {
+      return getScaledWidth(data.republicanDelegates) + 'px';
+    })
+    .style('background-color', '#0000bb');
 
   document.getElementById('switch').addEventListener('click', function() {
-    container.selectAll('div').data(delegateData)
-      .transition().duration(1000)
+    container.selectAll('.state-bar').data(delegateData)
+      .transition()
+      .duration(1000)
       .style('width', function(data, index) {
         if (republicans) {
           return getScaledWidth(data.democratDelegates) + 'px';
@@ -35,9 +52,11 @@ window.addEventListener('load', function() {
       });
 
     if (republicans) {
-      d3.select('#title').text('Democratic Delegates');
+      d3.select('#title')
+        .text('Democratic Delegates');
     } else {
-      d3.select('#title').text('Republican Delegates');
+      d3.select('#title')
+        .text('Republican Delegates');
     }
 
     republicans = !republicans;
